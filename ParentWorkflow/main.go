60f7d7764aa28d7ff42d51857f67688452533b94
Caleb Yarnell <caleb.yarnell@officedepot.com>
2022-04-22 11:24:35 -0500

    Ready for tutorial

--- ParentWorkflow/main.go
+++ ParentWorkflow/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"go.temporal.io/api/enums/v1"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 	"go.temporal.io/sdk/workflow"
 )
 
-func main(){
+func main() {
 	// Create the client object just once per process
 	c, err := client.NewClient(client.Options{})
 	if err != nil {
@@ -16,7 +15,7 @@ func main(){
 	}
 	defer c.Close()
 	// This worker hosts both Workflow and Activity functions
-	w := worker.New(c, "PTQ", worker.Options{})
+	w := worker.New(c, "Parent-Task-Queue", worker.Options{})
 	w.RegisterWorkflow(ParentWorkflow)
 	//w.RegisterActivity(app.ComposeGreeting)
 	// Start listening to the Task Queue
@@ -25,36 +24,6 @@ func main(){
 		fmt.Println("unable to start Worker", err)
 	}
 }
-func ParentWorkflow(ctx workflow.Context, id string) (string,error){
-	var signalObj string
-	isOrderFulfilled := false
-	c := workflow.GetSignalChannel(ctx, "error")
-	s := workflow.NewSelector(ctx)
-	s.AddReceive(c, func(c workflow.ReceiveChannel, more bool) {
-		c.Receive(ctx, &signalObj)
-		if signalObj == "Kill"{
-			isOrderFulfilled = true
-		}
-	})
-	logString := "Parent " + id
-	ctx = workflow.WithChildOptions(ctx, workflow.ChildWorkflowOptions{ParentClosePolicy: enums.PARENT_CLOSE_POLICY_ABANDON, TaskQueue: "C1TQ",WorkflowID: "C1WF"})
-	var cwfOne workflow.Execution
-	cwfOneFuture :=workflow.ExecuteChildWorkflow(ctx, "ChildWorkflowOne", id)
-	cwfOneFuture.GetChildWorkflowExecution().Get(ctx,cwfOne)
-	var cwfTwo workflow.Execution
-	ctx = workflow.WithChildOptions(ctx, workflow.ChildWorkflowOptions{ParentClosePolicy: enums.PARENT_CLOSE_POLICY_ABANDON, TaskQueue: "C2TQ", WorkflowID: "C2WF"})
-	cwfTwoFuture := workflow.ExecuteChildWorkflow(ctx, "ChildWorkflowTwo", id)
-	cwfTwoFuture.Get(ctx, cwfTwo)
-	s.AddFuture(cwfTwoFuture, func(f workflow.Future) {
-		var response string
-		f.Get(ctx, &response)
-		print(response)
-		isOrderFulfilled = true
-	})
-	for !isOrderFulfilled{
-		s.Select(ctx)
-	}
-	workflow.SignalExternalWorkflow(ctx, "C1WF","","kill","Kill").Get(ctx,nil)
-	return logString,nil
+func ParentWorkflow(ctx workflow.Context, inputString string) (string, error) {
+	return inputString + " World!", nil
 }
-
--- main.go
+++ main.go
@@ -8,26 +8,23 @@ import (
 func main() {
 	c, _ := client.NewClient(client.Options{})
 
-	createFPO(c)
-	//signal(c,"ack","this is an ack")
-	//signal(c, "asn", "this is an asn")
+	triggerWorkflow(c)
 	//signal(c, "asn", "Kill")
 
 }
 
-func createFPO(c client.Client) {
+// triggerWorkflow initiates a workflow
+func triggerWorkflow(c client.Client) {
 	options := client.StartWorkflowOptions{
-		ID:        "PworkflowCC",
-		TaskQueue: "PTQ",
+		// Name of the workflow
+		ID: "MyFirstWorkflow",
+		// THIS MUST MATCH THE TASK QUEUE YOUR WORKER IS LISTENING TO
+		TaskQueue: "Parent-Task-Queue",
 	}
-	c.ExecuteWorkflow(context.Background(), options, "ParentWorkflow", "123456")
+	_, _ = c.ExecuteWorkflow(context.Background(), options, "ParentWorkflow", "Hello")
 }
 
-func signal(c client.Client, workflow string, message string) {
-	switch workflow {
-	case "ack":
-		c.SignalWorkflow(context.Background(), "C1WF", "", workflow, message)
-	case "asn":
-		c.SignalWorkflow(context.Background(), "C2WF", "", workflow, message)
-	}
+// signal This signals a workflow by ID with a string message
+func signal(c client.Client, workflow string, channel string, message string) {
+	_ = c.SignalWorkflow(context.Background(), workflow, "", channel, message)
 }
